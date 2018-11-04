a65468540890d948d673042af203f3e68df65885
Victor Ribeiro <44476242+jvictorribeiro@users.noreply.github.com>
2018-11-04 14:35:42 -0300

    Add files via upload

--- sockServer.c
+++ sockServer.c
@@ -35,35 +35,36 @@ int main(){
 	local.sin_port				=htons(PORTA);		//a porta deve-se converter para endereço de rede utilizando a funcao htons
 	memset(local.sin_zero, 0x0, 8);			//setar o sin_zero para zero
 
-		if(bind(sockfd,(struct sockaddr*)&local,sizeof(local)) == -1){		//bind() une um nome ao socket que vc abriu -- bind(socket, estrutura de conexao, tam da estrutura)
-			perror("bind");
-			exit(1);
-		}
+	if(bind(sockfd,(struct sockaddr*)&local,sizeof(local)) == -1){		//bind() une um nome ao socket que vc abriu -- bind(socket, estrutura de conexao, tam da estrutura)
+		perror("bind");
+		exit(1);
+	}
 
-		listen(sockfd,1);		//listen() faz com que o socket aguarde por conexoes -- listen(socket, num max de conexoes)
+	listen(sockfd,1);		//listen() faz com que o socket aguarde por conexoes -- listen(socket, num max de conexoes)
 
-		if( (cliente = accept(sockfd,(struct sockaddr*)&remoto, &len)) == -1){ // descritor do cliente e canal para se comunicar com cliente
-			perror("accept");
-			exit(1);
-		}
+	if( (cliente = accept(sockfd,(struct sockaddr*)&remoto, &len)) == -1){ // descritor do cliente e canal para se comunicar com cliente
+		perror("accept");
+		exit(1);
+	}
 
-		strcpy(buffer,"Welcome!\n\0");
+	strcpy(buffer,"Welcome!\n\0");
 
-		if(send(cliente,buffer,strlen(buffer), 0)){		//send() usada para enviar uma msg para um socket -- send(socket, *msg, tam da msg, param adicionais)
-			printf("Aquardando resposta do cliente ...\n");
-			while(1){
-				memset(buffer,0x0,LEN);
-				if((slen = recv(cliente,buffer,LEN,0)) > 0){  //recv() msms parametros da send(), mas usada para receber uma msg
-					buffer[slen-1]='\0';	//limpar o buffer, caractere nulo no final
-					printf("Mensagem recebida: %s\n",buffer);
-					close(cliente);
-					break;
-				}
+	if(send(cliente,buffer,strlen(buffer), 0)){		//send() usada para enviar uma msg para um socket -- send(socket, *msg, tam da msg, param adicionais)
+		printf("Aquardando resposta do cliente ...\n");
+		while(1){
+			memset(buffer,0x0,LEN);
+			if((slen = recv(cliente,buffer,LEN,0)) > 0){  //recv() msms parametros da send(), mas usada para receber uma msg
+				buffer[slen-1]='\0';	//limpar o buffer, caractere nulo no final
+				printf("Mensagem recebida: %s\n",buffer);
+				close(cliente);
+				break;
 			}
 		}
+	}
 
-		close(sockfd);
-		printf("Servidor Encerrado!\n");
+	close(sockfd);
+	printf("Servidor Encerrado!\n");
 
-	return 0;
+return 0;
+
 }
