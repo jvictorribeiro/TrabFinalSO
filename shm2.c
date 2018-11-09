1a58cd0cffc21521e9ecc8508bda314a2caba3d3
Victor Ribeiro <44476242+jvictorribeiro@users.noreply.github.com>
2018-11-09 19:18:21 -0300

    shm2 edit
    shm 10 com 1 int final

--- shm2.c
+++ shm2.c
@@ -1,58 +1,71 @@
-// Ipc - mem partilhada 1 com 1
+// Ipc - mem partilhada 10 com 1
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <semaphore.h>
+#include <fcntl.h>
 
 int main(){
 	pid_t pid;
+	int *valor, i, j;
+	sem_t *produtor;
+  sem_t *organizador;
 
-	if((pid = fork()) < 0){
-		perror("fork");
-		exit(1);
-	}
-  if(pid > 0){
+	produtor = sem_open ("pSem", O_CREAT | O_EXCL, 0644, 1);
+	organizador = sem_open("oSem",O_CREAT | O_EXCL, 0322, 0);//e zro pq o produtor vai esperar o consumidor
+
+	for(i = 0; i < 10; ++i){
+    pid= fork();
+    if(pid == 0){ //se processo filho: sai do ciclo!
+      break;
+    }
+ 	}
+
+  if(pid == 0){
+		sem_wait(produtor);
     key_t key = ftok("shmfile",65); //ftok para gerar uma 'key' unica
 
     int shmid = shmget(key, 1024, 0666|IPC_CREAT);    //funcao shmget() para criar segmento de mem partilhada, ela retorna o id do segmento
                                                     //--shmget(key, tam em bytes do segmento, permicoes do segmento)
+    int *valor =  shmat(shmid,(void*)0,0);      //shmat() liga o processo ao segmento -- shmat(id do segmento, endereco do segmento, 0)
 
-    char *str = (char*) shmat(shmid,(void*)0,0);      //shmat() liga o processo ao segmento -- shmat(id do segmento, endereco do segmento, 0)
+		*valor = 10;
+		printf("dado escrito na memoria pelo filho: %d\n",valor[0]);
 
-    memcpy(str, "Hello!", 6);   //man page - memcpy() function copies n bytes from memory area src to memory area dest.
-    printf("dado escrito na memoria pelo pai: %s\n",str);
-
-    shmdt(str);  //desligar do segmento
+		shmdt(valor);  //desligar do segmento
+		sem_wait(organizador);
+		sem_post (produtor);
+		exit(0);
   }
-	else if(pid == 0){
-		pid_t childpid;
-		for(int i = 0; i < 10; i++){		// cria os dez filhos
-			if(( childpid = fork()) < 0){
-				perror("fork");
-				exit(1);
-			}
-			if(childpid == 0){
+	else{
+		for(j=0;j<10;j++){
     		key_t key = ftok("shmfile",65); //ftok para gerar uma 'key' unica
 
     		int shmid = shmget(key,1024,0666|IPC_CREAT);  //funcao shmget() para criar segmento de mem partilhada, ela retorna o id do segmento
                                                     //--shmget(key, tam em bytes do segmento, permicoes do segmento)
 
-				char *str = (char*) shmat(shmid,(void*)0,0);  //shmat() liga o processo ao segmento -- shmat(id do segmento, endereco do segmento, 0)
+				int *valor =  shmat(shmid,(void*)0,0);  //shmat() liga o processo ao segmento -- shmat(id do segmento, endereco do segmento, 0)
 
-    		printf("dado lido da memoria pelo filho %d: %s\n", i, str);
-
+    		printf("dado lido pelo pai do filho %d: %d\n", j, valor[0]);
+				sem_post(organizador);
 				if(i == 9){
-    			shmdt(str);     //desligar do segmento
+    			shmdt(valor);     //desligar do segmento
 					shmctl(shmid,IPC_RMID,NULL);    //destruir o segmento criado/
 				}
-				exit(0);
-			}
+				wait(NULL);
 		}
+	sem_unlink ("oSem");//fechar o semaforo
+	sem_close(organizador);
+	sem_unlink ("pSem");
+	sem_close(produtor);
 	}
+
+exit(0);
 
 return 0;
 
