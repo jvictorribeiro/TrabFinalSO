18aa4b24cc4af47e4d3e1d6e33ffe393f2d75ff0
Victor Ribeiro <44476242+jvictorribeiro@users.noreply.github.com>
2018-11-09 19:16:09 -0300

    pipe2 edit
    pipe 10 com 1 int final

--- pipe2.c
+++ pipe2.c
@@ -1,240 +1,68 @@
-//IPC - pipe 10 com 1
+//IPC - pipe 10 com 1]
 
+#include <stdio.h>          /* printf()                 */
+#include <stdlib.h>         /* exit(), malloc(), free() */
+#include <sys/types.h>      /* key_t, sem_t, pid_t      */
+#include <sys/shm.h>        /* shmat(), IPC_RMID        */
+#include <errno.h>          /* errno, ECHILD            */
+#include <semaphore.h>      /* sem_open(), sem_destroy(), sem_wait().. */
+#include <fcntl.h>
 #include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <sys/wait.h>
 
 int main(){
-  int i, n =10;
-  pid_t pid,pid2,pid3,pid4,pid5,pid6,pid7,pid8,pid9;
-  int fd[2], fd1[2], fd2[2], fd3[2], fd4[2], fd5[2], fd6[2], fd7[2], fd8[2], fd9[2];
+  sem_t *produtor;
+  sem_t *organizador;
+  int i,x;
+  pid_t pid;
+  int fd[2];
 
-  if(pipe(fd)<0){ //cria o pipe1
-    perror("pipe");
-    return -1;
-  }
+ if(pipe(fd) < 0){    //cria o pipe
+   perror("pipe");    //funcao pipe retorna -1 em caso de erro
+   return -1;
+ }
+ produtor = sem_open ("pSem", O_CREAT | O_EXCL, 0644, 1);
+ organizador=sem_open("oSem",O_CREAT | O_EXCL, 0322, 0);//e zro pq o produtor vai esperar o consumidor
 
-  if(pipe(fd1)<0){  //cria o pipe2
-    perror("pipe");
-    return -1;
-  }
-
-  if(pipe(fd2)<0){  //cria o pipe3
-    perror("pipe");
-    return -1;
-  }
-
-  if(pipe(fd3)<0){  //cria o pipe4
-    perror("pipe");
-    return -1;
-  }
-
-  if(pipe(fd4)<0){  //cria o pipe5
-    perror("pipe");
-    return -1;
-  }
-
-  if(pipe(fd5)<0){  //cria o pipe6
-    perror("pipe");
-    return -1;
-  }
-
-  if(pipe(fd6)<0){  //cria o pipe7
-    perror("pipe");
-    return -1;
-  }
-
-  if(pipe(fd7)<0){  //cria o pipe8
-    perror("pipe");
-    return -1;
-  }
-
-  if(pipe(fd8)<0){  //cria o pipe9
-    perror("pipe");
-    return -1;
-  }
-
-  if(pipe(fd9)<0){  //cria o pipe10
-    perror("pipe");
-    return -1;
-  }
-
- for(i = 0; i < n; ++i){
+ for(i = 0; i < 10; ++i){
    pid= fork();
-   if(pid != 0){ //se processo filho: sai do ciclo!
+   if(pid == 0){ //se processo filho: sai do ciclo!
      break;
    }
  }
 
- if(pid > 0 && i==0){
-   close(fd[0]);//fecha a leitura do pipe, ja que sera usada a escrita fd[1]
-   char msg[200]="Hello1!!!";
-   printf("Enviada pelo processo 1 no pipe:'%s'\n",msg);
+ if(pid == 0){  //processo filho
+   sem_wait (produtor);
+   close(fd[0]);      //fecha a leitura do pipe, ja que sera usada a escrita fd[1]
+   int msg[1] = {10};
+   printf("String enviada pelo pai no pipe: %i\n",msg[0]);
 
-   write(fd[1],msg,sizeof(msg)+1);//escreve a mensagem no fd[1]
-
- }else if(pid ==0){
-   char msg_recebida[200];
+   write(fd[1],msg,sizeof(msg));  //escreve a mensagem no pipe, funcao write recebe o file descriptors, um ponteiro e o num de bytes +1 por causa do \0
+   sem_wait(organizador);
+   printf("acabo produtor\n");
+   sem_post (produtor);           /* V operation */
    close(fd[1]);//fecha a escrita do pipe, ja que sera usada a leitura fd[0]
+   exit(0);
 
-   read(fd[0],msg_recebida,sizeof(msg_recebida));//leitura do que foi escrito
+ }else{    //processo pai
+     for(x=0;x<10;x++){
+     int msg_recebida;
+     close(fd[1]);    //fecha a escrita do pipe, ja que sera usada a leitura fd[0]
 
-   printf("string recebida pelo processo 0 no pipe:'%s'\n",msg_recebida);
-  }
+     read(fd[0],&msg_recebida,sizeof(msg_recebida));   //leitura do que foi escrito no pipe
 
-  if(pid > 0 && i==1){
-    close(fd1[0]);//fecha a leitura do pipe, ja que sera usada a escrita fd[1]
-    char msg[200]="Hello2!!!";
-    printf("Enviada pelo processo 2 no pipe:'%s'\n",msg);
+     printf("String lida pelo filho %d no pipe: %d\n",x,msg_recebida);
+     sem_post(organizador);
+   }
+   close(fd[0]);//fecha a leitura do pipe, ja que sera usada a escrita fd[1]
 
-    write(fd1[1],msg,sizeof(msg)+1);//escreve a mensagem no fd[1]
-    wait(NULL);
+   sem_unlink ("oSem");//fechar o semaforo
+   sem_close(organizador);
+   sem_unlink ("pSem");
+   sem_close(produtor);
 
-  }else if(pid ==0){
-    char msg_recebida[200];
-    close(fd1[1]);//fecha a escrita do pipe, ja que sera usada a leitura fd[0]
-
-    read(fd1[0],msg_recebida,sizeof(msg_recebida));//leitura do que foi escrito
-
-    printf("string recebida pelo processo 0 no pipe:'%s'\n",msg_recebida);
-  }
-
-  if(pid > 0 && i==2){
-    close(fd2[0]);//fecha a leitura do pipe, ja que sera usada a escrita fd[1]
-    char msg[200]="Hello3!!!";
-    printf("Enviada pelo processo 3 no pipe:'%s'\n",msg);
-
-    write(fd2[1],msg,sizeof(msg)+1);//escreve a mensagem no fd[1]
-    wait(NULL);
-
-  }else if(pid ==0){
-    char msg_recebida[200];
-    close(fd2[1]);//fecha a escrita do pipe, ja que sera usada a leitura fd[0]
-
-    read(fd2[0],msg_recebida,sizeof(msg_recebida));//leitura do que foi escrito
-
-    printf("string recebida pelo processo 0 no pipe:'%s'\n",msg_recebida);
-  }
-
-  if(pid > 0 && i==3){
-    close(fd3[0]);//fecha a leitura do pipe, ja que sera usada a escrita fd[1]
-    char msg[200]="Hello4!!!";
-    printf("Enviada pelo processo 4 no pipe:'%s'\n",msg);
-
-    write(fd3[1],msg,sizeof(msg)+1);//escreve a mensagem no fd[1]
-    wait(NULL);
-
-  }else if(pid ==0){
-    char msg_recebida[200];
-    close(fd3[1]);//fecha a escrita do pipe, ja que sera usada a leitura fd[0]
-
-    read(fd3[0],msg_recebida,sizeof(msg_recebida));//leitura do que foi escrito
-
-    printf("string recebida pelo processo 0 no pipe:'%s'\n",msg_recebida);
-  }
-
-  if(pid > 0 && i==4){
-    close(fd4[0]);//fecha a leitura do pipe, ja que sera usada a escrita fd[1]
-    char msg[200]="Hello5!!!";
-    printf("Enviada pelo processo 5 no pipe:'%s'\n",msg);
-
-    write(fd4[1],msg,sizeof(msg)+1);//escreve a mensagem no fd[1]
-    wait(NULL);
-
-  }else if(pid ==0){
-    char msg_recebida[200];
-    close(fd4[1]);//fecha a escrita do pipe, ja que sera usada a leitura fd[0]
-
-    read(fd4[0],msg_recebida,sizeof(msg_recebida));//leitura do que foi escrito
-
-    printf("string recebida pelo processo 0 no pipe:'%s'\n",msg_recebida);
-  }
-
-  if(pid > 0 && i==5){
-    close(fd5[0]);//fecha a leitura do pipe, ja que sera usada a escrita fd[1]
-    char msg[200]="Hello6!!!";
-    printf("Enviada pelo processo 6 no pipe:'%s'\n",msg);
-
-    write(fd5[1],msg,sizeof(msg)+1);//escreve a mensagem no fd[1]
-    wait(NULL);
-
-  }else if(pid ==0){
-    char msg_recebida[200];
-    close(fd5[1]);//fecha a escrita do pipe, ja que sera usada a leitura fd[0]
-
-    read(fd5[0],msg_recebida,sizeof(msg_recebida));//leitura do que foi escrito
-
-    printf("string recebida pelo processo 0 no pipe:'%s'\n",msg_recebida);
-  }
-
-  if(pid > 0 && i==6){
-    close(fd6[0]);//fecha a leitura do pipe, ja que sera usada a escrita fd[1]
-    char msg[200]="Hello7!!!";
-    printf("Enviada pelo processo 7 no pipe:'%s'\n",msg);
-
-    write(fd6[1],msg,sizeof(msg)+1);//escreve a mensagem no fd[1]
-    wait(NULL);
-
-  }else if(pid ==0){
-    char msg_recebida[200];
-    close(fd6[1]);//fecha a escrita do pipe, ja que sera usada a leitura fd[0]
-
-    read(fd6[0],msg_recebida,sizeof(msg_recebida));//leitura do que foi escrito
-
-    printf("string recebida pelo processo 0 no pipe:'%s'\n",msg_recebida);
-  }
-
-  if(pid > 0 && i==7){
-    close(fd7[0]);//fecha a leitura do pipe, ja que sera usada a escrita fd[1]
-    char msg[200]="Hello8!!!";
-    printf("Enviada pelo processo 8 no pipe:'%s'\n",msg);
-
-    write(fd7[1],msg,sizeof(msg)+1);//escreve a mensagem no fd[1]
-    wait(NULL);
-
-  }else if(pid ==0){
-    char msg_recebida[200];
-    close(fd7[1]);//fecha a escrita do pipe, ja que sera usada a leitura fd[0]
-
-    read(fd7[0],msg_recebida,sizeof(msg_recebida));//leitura do que foi escrito
-
-    printf("string recebida pelo processo 0 no pipe:'%s'\n",msg_recebida);
-  }
-
-  if(pid > 0 && i==8){
-    close(fd8[0]);//fecha a leitura do pipe, ja que sera usada a escrita fd[1]
-    char msg[200]="Hello9!!!!";
-    printf("Enviada pelo processo 9 no pipe:'%s'\n",msg);
-
-    write(fd8[1],msg,sizeof(msg)+1);//escreve a mensagem no fd[1]
-    wait(NULL);
-
-  }else if(pid ==0){
-    char msg_recebida[200];
-    close(fd8[1]);//fecha a escrita do pipe, ja que sera usada a leitura fd[0]
-
-    read(fd8[0],msg_recebida,sizeof(msg_recebida));//leitura do que foi escrito
-
-    printf("string recebida pelo processo 0 no pipe:'%s'\n",msg_recebida);
-  }
-
-  if(pid > 0 && i==9){
-    close(fd9[0]);//fecha a leitura do pipe, ja que sera usada a escrita fd[1]
-    char msg[200]="Hello10!!!";
-    printf("Enviada pelo processo 10 no pipe:'%s'\n",msg);
-
-    write(fd9[1],msg,sizeof(msg)+1);//escreve a mensagem no fd[1]
-    wait(NULL);
-
-  }else if(pid ==0){
-    char msg_recebida[200];
-    close(fd9[1]);//fecha a escrita do pipe, ja que sera usada a leitura fd[0]
-
-    read(fd9[0],msg_recebida,sizeof(msg_recebida));//leitura do que foi escrito
-
-    printf("string recebida pelo processo 0 no pipe:'%s'\n",msg_recebida);
-  }
-
+ }
+ exit(0);
 return 0;
+
 }
