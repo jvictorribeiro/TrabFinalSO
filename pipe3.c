1532b1a02f5181817dea0d14a59c3c06f2b79c66
Victor Ribeiro <44476242+jvictorribeiro@users.noreply.github.com>
2018-11-10 14:45:23 -0300

    pipe 10 com 10 final

--- pipe3.c
+++ pipe3.c
@@ -1,87 +1,88 @@
-//IPC - pipe 10 com 1]
+//IPC - pipe 10 com 10
 
-#include <stdio.h>          /* printf()                 */
-#include <stdlib.h>         /* exit(), malloc(), free() */
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>      /* key_t, sem_t, pid_t      */
 #include <sys/shm.h>        /* shmat(), IPC_RMID        */
 #include <errno.h>          /* errno, ECHILD            */
+
+//headers para semaforo -- rodar com -lpthread
 #include <semaphore.h>      /* sem_open(), sem_destroy(), sem_wait().. */
 #include <fcntl.h>
+
 #include <unistd.h>
 #include <sys/wait.h>
 
 int main(){
   sem_t *produtor;
-  sem_t *organizador;
-	sem_t *sincro;
-  int i,x;
+  sem_t *consumidor;
+	sem_t *sincro;   //semaforo usado para sincronizacao
+  int i,j;
   pid_t pid;
   int fd[2];
 
- if(pipe(fd) < 0){    //cria o pipe
-   perror("pipe");    //funcao pipe retorna -1 em caso de erro
-   return -1;
- }
- produtor = sem_open ("pSem", O_CREAT | O_EXCL, 0644, 1);
- organizador=sem_open("oSem",O_CREAT | O_EXCL, 0322, 1);//e zro pq o produtor vai esperar o consumidor
- sincro= sem_open ("sSem", O_CREAT | O_EXCL, 0144, 0);
+  if(pipe(fd) < 0){    //cria o pipe
+    perror("pipe");    //funcao pipe retorna -1 em caso de erro
+    return -1;
+  }
 
- for(i = 0; i < 20; ++i){
-   pid= fork();
-   if(pid == 0){ //se processo filho: sai do ciclo!
-     break;
-   }
- }
+  produtor = sem_open ("pSem", O_CREAT | O_EXCL, 0644, 1);  //cria os semaforos, o ultimo argumento eh o valor que vai definir se espera
+  consumidor = sem_open("qSem",O_CREAT | O_EXCL, 0322, 1);
+  sincro= sem_open ("rSem", O_CREAT | O_EXCL, 0144, 0);
 
- if(pid > 0){  //processo filho
-	 /* Esperando todos os filhos saire, afinal, o ultimo processo a sair sera um filho */
-				while(pid=waitpid(-1, NULL, 0))	/* Espera pelo processo término do processo filho até que */
-				{
-					if(errno == ECHILD)			/* Nenhum processo filho exista mais, entao errno e atualizado com ECHILD */
-						break;
-				}
-   printf("Todos os filhos sairam\n");
-	 sem_unlink ("oSem");//fechar o semaforo
-   sem_close(organizador);
-   sem_unlink ("pSem");
-   sem_close(produtor);
-	 sem_unlink("sSem");
-	 sem_close(sincro);
-	 exit(0);
+  for(i = 0; i < 20; ++i){    //cria 20 filhos
+    pid= fork();
+    if(pid == 0){ //se processo filho: sai do ciclo!
+      break;
+    }
+  }
 
- }else{    //processo pai
-	 if(i<10){
+  if(pid > 0){  //processo pai
+	  /* Esperando todos os filhos sairem*/
+	  while(pid = waitpid(-1, NULL, 0)){	/* Espera pelo processo término do processo filho até que */
+		    if(errno == ECHILD)			/* Nenhum processo filho exista mais, entao errno e atualizado com ECHILD */
+				    break;
+		}
+    printf("Todos os filhos sairam\n");
 
-     for(x=0;x<10;x++){
-		 	close(fd[1]);    //fecha a escrita do pipe, ja que sera usada a leitura fd[0]
-			sem_wait(organizador);
-			sem_post(sincro);
+    sem_unlink ("pSem");    //fecha os semaforos
+    sem_close(produtor);
+    sem_unlink ("qSem");
+    sem_close(consumidor);
+	  sem_unlink("rSem");
+	  sem_close(sincro);
+	  exit(0);
+  }else{    //processo filho
+    if(i < 10){
+      close(fd[1]);    //fecha a escrita do pipe, ja que sera usada a leitura fd[0]
+			sem_wait(consumidor);    //decrementa valor do semaforo, trava
+
      	int msg_recebida;
 
+      read(fd[0], &msg_recebida, sizeof(msg_recebida));   //leitura do que foi escrito no pipe
 
-     read(fd[0],&msg_recebida,sizeof(msg_recebida));   //leitura do que foi escrito no pipe
+      printf("valor lido pelo filho %d no pipe: %d\n",i ,msg_recebida);
+		  sem_post(produtor);   //incrementa valor do semaforo, libera
+      sem_post(sincro);     //incrementa semaforo para sincronizacao
+      close(fd[0]);//fecha a leitura do pipe, ja que sera usada a escrita fd[1]
+    }else{
+      close(fd[0]);      //fecha a leitura do pipe, ja que sera usada a escrita fd[1]
+	    sem_wait (produtor);
+      int msg[1] = {10};
+      printf("valor escrito pelo filho %d no pipe: %i\n",i,msg[0]);
 
-     printf("String lida pelo filho %d no pipe: %d\n",x,msg_recebida);
-    sem_post(organizador);
-   }
-   close(fd[0]);//fecha a leitura do pipe, ja que sera usada a escrita fd[1]
- }else{
-	 close(fd[0]);      //fecha a leitura do pipe, ja que sera usada a escrita fd[1]
-	 sem_wait (produtor);
-   int msg[1] = {10};
-   printf("String enviada pelo processo %d no pipe: %i\n",i,msg[0]);
-	 sem_wait(sincro);
-   write(fd[1],msg,sizeof(msg));  //escreve a mensagem no pipe, funcao write recebe o file descriptors, um ponteiro e o num de bytes +1 por causa do \0
-   sem_post(produtor);
-   //printf("acabo produtor\n");           /* V operation */
-   close(fd[1]);//fecha a escrita do pipe, ja que sera usada a leitura fd[0]
-   exit(0);
+      write(fd[1], msg, sizeof(msg));  //escreve a mensagem no pipe, funcao write recebe o file descriptors, um ponteiro e o num de bytes +1 por causa do \0
+      sem_post(organizador);
+      sem_wait(sincro);
 
- }
+      //printf("acabo produtor\n");
+      close(fd[1]);   //fecha a escrita do pipe
+      exit(0);
+    }
+  }
 
+exit(0);
 
- }
- exit(0);
 return 0;
 
 }
