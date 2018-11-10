047e1a972101e946007d087b96d7a596c030a01f
Victor Ribeiro <44476242+jvictorribeiro@users.noreply.github.com>
2018-11-10 15:48:35 -0300

    sock 1 com 1 final

--- sock1.c
+++ sock1.c
@@ -21,80 +21,75 @@ int main(){
 		exit(1);
 	}
 
-	if(pid > 0){
-    int sockfd, new_socket, valorLido;
+	if(pid > 0){		//pai
+  	int sockfd, new_socket, valorLido;
     struct sockaddr_in endereco;
     int opt = 1;    //valor de opt
     int addrlen = sizeof(endereco);    //tam do endereço
-    int buffer[1]={10};
+  	int buffer[1]={10};
 
+		if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == 0){   //criando o descriptor do socket
+    	perror("socket falhou");
+      exit(1);
+   	}
 
-   if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == 0){   //criando o descriptor do socket
-       perror("socket falhou");
-       exit(1);
-   }
+   	if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))){    //opcional, mas ajuda a manipular as opcoes para o socket referido
+    	perror("setsockopt");
+      exit(EXIT_FAILURE);
+   	}
 
-   if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))){    //opcional, mas ajuda a manipular as opcoes para o socket referido
-       perror("setsockopt");
-       exit(EXIT_FAILURE);
-   }
+   	endereco.sin_family = AF_INET;
+   	endereco.sin_addr.s_addr = INADDR_ANY;
+   	endereco.sin_port = htons(PORTA);
 
-   endereco.sin_family = AF_INET;
-   endereco.sin_addr.s_addr = INADDR_ANY;
-   endereco.sin_port = htons(PORTA);
+   	if(bind(sockfd, (struct sockaddr *)&endereco, sizeof(endereco)) < 0){  //liga o socket a porta 5000
+    	perror("bind falhou");
+      exit(1);
+   	}
 
-   if(bind(sockfd, (struct sockaddr *)&endereco, sizeof(endereco)) < 0){  //liga o socket a porta 5000
-       perror("bind falhou");
-       exit(1);
-   }
+   	if(listen(sockfd, 1) < 0){   //aguarda conexoes do cliente
+    	perror("listen");
+      exit(1);
+   	}
 
-   if(listen(sockfd, 1) < 0){   //aguarda conexoes do cliente
-       perror("listen");
-       exit(1);
-   }
+   	if((new_socket = accept(sockfd, (struct sockaddr *)&endereco,(socklen_t*)&addrlen)) < 0){		//aceita conexao de clientes
+    	perror("accept");
+      exit(1);
+   	}
 
-   if((new_socket = accept(sockfd, (struct sockaddr *)&endereco,(socklen_t*)&addrlen)) < 0){
-       perror("accept");
-       exit(1);
-   }
+   	printf("Servidor enviou: ");
+   	send(new_socket ,(char*)buffer , 1*sizeof(int) , 0 );   //envia a msg
+   	printf("%d\n",buffer[0] );
 
-   printf("Servidor enviou: ");
-   send(new_socket ,(char*)buffer , 1*sizeof(int) , 0 );   //envia a msg
-   printf("%d\n",buffer[0] );
+	}else if(pid == 0){  //filho
+		struct sockaddr_in endereco;
+   	int clientSock = 0, valorLido;
+   	struct sockaddr_in enderecoServer;
+   	int buffer[1] = {0};
 
+   	if((clientSock = socket(AF_INET, SOCK_STREAM, 0)) < 0){    //ciente cria o socket
+    	printf("\n erro na criacao do socket \n");
+      return -1;
+   	}
 
- }else if(pid == 0){
+   	memset(&enderecoServer, '0', sizeof(enderecoServer));
 
-   struct sockaddr_in endereco;
-   int clientSock = 0, valorLido;
-   struct sockaddr_in enderecoServer;
-   int buffer[1] = {0};
+   	enderecoServer.sin_family = AF_INET;
+   	enderecoServer.sin_port = htons(PORTA);
 
-   if((clientSock = socket(AF_INET, SOCK_STREAM, 0)) < 0){    //ciente cria o socket igual ao servidor
-       printf("\n erro na criacao do socket \n");
-       return -1;
-   }
+   	if(inet_pton(AF_INET, "127.0.0.1", &enderecoServer.sin_addr) <= 0){    //inet pton converte enderecos para forma binaria
+    	printf("\nEndereco invalido\n");
+      return -1;
+   	}
 
-   memset(&enderecoServer, '0', sizeof(enderecoServer));
-
-   enderecoServer.sin_family = AF_INET;
-   enderecoServer.sin_port = htons(PORTA);
-
-   if(inet_pton(AF_INET, "127.0.0.1", &enderecoServer.sin_addr) <= 0){    //inet pton converte enderecos para forma binaria
-       printf("\nEndereco invalido\n");
-       return -1;
-   }
-
-   if(connect(clientSock, (struct sockaddr *)&enderecoServer, sizeof(enderecoServer)) < 0){ //conecta o socket referenciado, ao endereco especificado
-       printf("\nConexao falhou \n");
-       return -1;
-   }
-
-
-   printf("Cliente recebeu: ");
-   recv( clientSock , buffer, 1*sizeof(int),0);    //msg lida por ele
-   printf("%d\n",buffer[0]);
- }
+   	if(connect(clientSock, (struct sockaddr *)&enderecoServer, sizeof(enderecoServer)) < 0){ //conecta o socket referenciado, ao endereco especificado
+    	printf("\nConexao falhou \n");
+      return -1;
+   	}
+		printf("Cliente recebeu: ");
+   	recv( clientSock , buffer, 1*sizeof(int),0);    //msg lida por ele
+   	printf("%d\n",buffer[0]);
+	}
 
 return 0;
 
